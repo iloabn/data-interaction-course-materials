1f9258b1353c9905a5862cdcd7c34bbbadcaf0a7
Fabian Miiro <fabian.miiro@if.se>
2022-03-29 11:35:51 +0200

    Easy way out

--- examples/07-woofwoof-client/src/config.js
+++ examples/07-woofwoof-client/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "http://localhost:4649";
+// NOT AN ENVIRONMENT VARIABLE
+// THIS THING *WILL* BE ON PEOPLES BROWSERS
+export const API_BASE_URL = "https://woofwoof-api.herokuapp.com";
